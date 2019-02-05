c8bd1690347d1d142e8dd22227d7f89e34f1bc47
glen <glen_sale@yahoo.com>
2019-02-05 10:08:32 -0800

    it will add components file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoComponent } from './video/video.component';
+import { HomeLoginComponent } from './home-login/home-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoComponent,
+    HomeLoginComponent
   ],
   imports: [
     BrowserModule
